8cb72786f888a2d4e458571422cffd36420d67ab
mmarkelov <maks-markel@mail.ru>
2019-05-19 15:28:17 +0300

    Rewrite ActionSheet with hooks

--- src/components/ActionSheet/ActionSheet.js
+++ src/components/ActionSheet/ActionSheet.js
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PopoutWrapper from '../PopoutWrapper/PopoutWrapper';
 import getClassName from '../../helpers/getClassName';
@@ -9,42 +9,22 @@ import withInsets from '../../hoc/withInsets';
 
 const baseClassNames = getClassName('ActionSheet');
 
-class ActionSheet extends React.Component {
-  state = {
-    closing: false
+const ActionSheet = props => {
+  const el = React.createRef();
+  const [closing, setClosing] = useState(false);
+
+  const onClose = () => {
+    setClosing(true);
+    waitTransitionFinish(props.onClose);
   };
 
-  static propTypes = {
-    /**
-     * iOS only
-     */
-    title: PropTypes.node,
-    /**
-     * iOS only
-     */
-    text: PropTypes.node,
-    onClose: PropTypes.func.isRequired,
-    style: PropTypes.object,
-    children: PropTypes.node,
-    className: PropTypes.string,
-    /**
-     * @ignore
-     */
-    insets: PropTypes.object
-  };
-
-  onClose = () => {
-    this.setState({ closing: true });
-    this.waitTransitionFinish(this.props.onClose);
-  };
-
-  onItemClick = (action, autoclose) => (event) => {
+  const onItemClick = (action, autoclose) => event => {
     event.persist();
 
     if (autoclose) {
-      this.setState({ closing: true });
-      this.waitTransitionFinish(() => {
-        autoclose && this.props.onClose();
+      setClosing(true);
+      waitTransitionFinish(() => {
+        autoclose && props.onClose();
         action && action(event);
       });
     } else {
@@ -52,52 +32,67 @@ class ActionSheet extends React.Component {
     }
   };
 
-  getRef = el => this.el = el;
+  const stopPropagation = e => e.stopPropagation();
 
-  stopPropagation = e => e.stopPropagation();
-
-  waitTransitionFinish (eventHandler) {
+  const waitTransitionFinish = eventHandler => {
     if (transitionEvents.supported) {
       const eventName = transitionEvents.prefix ? transitionEvents.prefix + 'TransitionEnd' : 'transitionend';
 
-      this.el.removeEventListener(eventName, eventHandler);
-      this.el.addEventListener(eventName, eventHandler);
+      el.current.removeEventListener(eventName, eventHandler);
+      el.current.addEventListener(eventName, eventHandler);
     } else {
       setTimeout(eventHandler.bind(this), IS_PLATFORM_ANDROID ? 200 : 300);
     }
-  }
+  };
 
-  render () {
-    const { children, className, title, text, style, insets, ...restProps } = this.props;
+  const { children, className, title, text, style, insets, ...restProps } = props;
 
-    return (
-      <PopoutWrapper
-        closing={this.state.closing}
-        v={IS_PLATFORM_IOS ? 'bottom' : 'center'}
-        h="center"
-        className={className}
-        style={style}
-        onClick={this.onClose}
-      >
-        <div {...restProps} ref={this.getRef} onClick={this.stopPropagation} className={classNames(baseClassNames, {
-          'ActionSheet--closing': this.state.closing
-        })}>
-          {IS_PLATFORM_IOS &&
+  return (
+    <PopoutWrapper
+      closing={closing}
+      v={IS_PLATFORM_IOS ? 'bottom' : 'center'}
+      h="center"
+      className={className}
+      style={style}
+      onClick={onClose}
+    >
+      <div {...restProps} ref={el} onClick={stopPropagation} className={classNames(baseClassNames, {
+        'ActionSheet--closing': closing
+      })}>
+        {IS_PLATFORM_IOS &&
           <header className="ActionSheet__header">
             {title && <div className="ActionSheet__title">{title}</div>}
             {text && <div className="ActionSheet__text">{text}</div>}
           </header>
-          }
-          {Children.toArray(children).map((Child, index, arr) => (
-            Child && React.cloneElement(Child, {
-              onClick: this.onItemClick(Child.props.onClick, Child.props.autoclose),
-              style: index === arr.length - 1 && insets.bottom ? { marginBottom: insets.bottom } : null
-            })
-          ))}
-        </div>
-      </PopoutWrapper>
-    );
-  }
-}
+        }
+        {React.Children.toArray(children).map((Child, index, arr) => (
+          Child && React.cloneElement(Child, {
+            onClick: onItemClick(Child.props.onClick, Child.props.autoclose),
+            style: index === arr.length - 1 && insets.bottom ? { marginBottom: insets.bottom } : null
+          })
+        ))}
+      </div>
+    </PopoutWrapper>
+  );
+};
+
+ActionSheet.propTypes = {
+  /**
+   * iOS only
+   */
+  title: PropTypes.node,
+  /**
+   * iOS only
+   */
+  text: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+  style: PropTypes.object,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  /**
+   * @ignore
+   */
+  insets: PropTypes.object
+};
 
 export default withInsets(ActionSheet);
